beab402bb98a96fd7707b306b42e083e79d704df
soonjongnim <soonjongnim@naver.com>
2023-03-23 00:36:27 +0900

    ㅁㅁ

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('./index.html')
+    result = 1 + 1
+    return render_template('./index.html', calc_result = result)

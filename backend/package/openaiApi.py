e26b22971b2ecadb9a72d1d0e60b6815cbab275a
soon9086 <soonjongnim@naver.com>
2023-04-21 16:22:36 +0900

    apikey변경
    Signed-off-by: soon9086 <soonjongnim@naver.com>

--- backend/package/openaiApi.py
+++ backend/package/openaiApi.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = "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"
+openai.api_key = "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"
 messages = [
 	{"role": "system", "content": "당신은 세계 최고의 번역가입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗번랭입니다. 당신은 어떤언어든 매우 명확하게 번역하고 타당한 번역을 줄 수 있습니다. 번역 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
 	{"role": "user", "content": "당신은 세계 최고의 번역가입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗번랭입니다. 당신은 어떤언어든 매우 명확하게 번역하고 타당한 번역을 줄 수 있습니다. 번역 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
@@ -10,8 +10,10 @@ messages = [
 
 def openaiApiFunc(content, languages):
 	print('openaiApiFunc content: ')
-	last_sentence = "라인별로 " + languages + "로 번역해줘"
+	print("messages: " , languages)
+	last_sentence = "'\n' 구분하여 한 라인씩 " + languages + "로 번역해주고 한줄씩나오게해줘"
 	messages.append({"role": "user", "content": '```' + content + '```' + last_sentence})
+	print("messages: " , messages)
 	completion = openai.ChatCompletion.create(
 		model="gpt-3.5-turbo",
 		messages=messages
@@ -19,7 +21,6 @@ def openaiApiFunc(content, languages):
 	assistant_content = completion.choices[0].message["content"]
 	# messages.append({"role": "assistant", "content": assistant_content})
 
-	print('```' + content + '```')
 	print(assistant_content)
 
 	return assistant_content
